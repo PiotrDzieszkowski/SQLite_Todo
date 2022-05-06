09d3b1732b4f5f0af9650f35e78e5c4b825b64ef
Piotr Dzieszkowski <piotr.dzieszkowski@gmail.com>
2022-05-06 20:14:47 +0100

    sqlite_todo

---
+++ create_table.py
@@ -0,0 +1,22 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_table():
+    conn = None
+    db_file = "database.db"
+    try:
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS todos (
+            id integer PRIMARY KEY, 
+            title VARCHAR(250) NOT NULL, 
+            description text NOT NULL,
+            done Boolean,
+            rating TEXT NOT NULL);''')
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+create_table()
---
+++ sqlite_models.py
@@ -0,0 +1,61 @@
+import sqlite3
+from sqlite3 import Error
+
+class TodosSQLite:
+    def __init__(self):
+        self.conn = None
+        try:
+            self.conn = sqlite3.connect("database.db")
+        except Error as e:
+            print(e) 
+
+    def create_connection(self):
+        try:
+            self.conn = sqlite3.connect("database.db")
+            return self.conn
+        except Error as e:
+            print(e)    
+        return self.conn
+
+    def all(self):
+        with self.conn as conn:
+            cur = conn.cursor()
+            cur.execute(f"SELECT * FROM todos")
+        return cur.fetchall()
+    
+    def get(self, id):
+        with self.conn as conn:
+            cur = conn.cursor()
+            cur.execute(f"SELECT * FROM todos WHERE id={id}")
+            row = cur.fetchone()
+            dict = {"id": row[0], "title": row[1], "description": row[2], "done": row[3], "raing": row[4]}
+        return dict
+    
+    def create(self, data):
+        data.pop('csrf_token')
+        with self.conn as conn:
+            cur = conn.cursor()
+            sql = """INSERT INTO todos (title, description, done, rating) VALUES (?,?,?,?)"""
+            values = []
+            for val in data.values():
+                values.append(val)
+            cur.execute(sql,values)
+            conn.commit()
+        return cur.lastrowid
+
+    def update(self, id, data):
+        data.pop('csrf_token')
+        with self.conn as conn:
+            cur = conn.cursor()
+            parameters = [f"{k} = ?" for k in data.keys()]
+            parameters = ", ".join(parameters)
+            values = tuple(v for v in data.values())
+            values += (id, )
+            sql = f''' UPDATE todos
+                        SET {parameters}
+                        WHERE id = ?'''            
+            try:
+                cur.execute(sql, values)
+                conn.commit()
+            except sqlite3.OperationalError as e:
+                print(e)
